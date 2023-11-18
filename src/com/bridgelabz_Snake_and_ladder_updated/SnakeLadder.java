b17f01a11eec2da919645beb883a9a8316993500
Sanket Garde <sanketgarde7473@gmail.com>
2023-11-18 23:31:04 +0530

    Added:Java functionality for Snake and Ladder single player start position 0

--- src/com/bridgelabz_Snake_and_ladder_updated/SnakeLadder.java
+++ src/com/bridgelabz_Snake_and_ladder_updated/SnakeLadder.java
@@ -4,5 +4,6 @@ public class SnakeLadder {
     public static void main(String[] args)
     {
         System.out.println("Playing Snake and Ladder");
+        System.out.println("Single Player start at 0 position ");
     }
 }
