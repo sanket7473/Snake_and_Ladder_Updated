5da3cdd6b9e54cb54f0dce1dda41d24929fd0bc9
Sanket Garde <sanketgarde7473@gmail.com>
2023-11-18 23:26:11 +0530

    Added:Java functionality for Snake and Ladder Welcome Message

---
+++ src/com/bridgelabz_Snake_and_ladder_updated/SnakeLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz_Snake_and_ladder_updated;
+
+public class SnakeLadder {
+    public static void main(String[] args)
+    {
+        System.out.println("Playing Snake and Ladder");
+    }
+}
